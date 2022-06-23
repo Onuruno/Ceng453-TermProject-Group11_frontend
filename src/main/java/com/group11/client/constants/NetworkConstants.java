e0d7e3713c360fc40447e25d646742bc1dffd151
Onuruno <onur_gokce@windowslive.com>
2022-06-23 13:30:48 +0300

    Heroku link added.

--- src/main/java/com/group11/client/constants/NetworkConstants.java
+++ src/main/java/com/group11/client/constants/NetworkConstants.java
@@ -1,7 +1,7 @@
 package com.group11.client.constants;
 
 public class NetworkConstants {
-    public static final String API = "http://localhost:8080/";
+    public static final String API = "https://ceng453-term-project-group11v2.herokuapp.com/";
 
     public static final String WEEKLY = "weekly";
 
